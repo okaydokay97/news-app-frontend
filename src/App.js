0c3494f310dcb654e19abc3bbb7244137c7b77a7
okaydokay97 <rubens.fernandes97@gmail.com>
2022-07-14 14:20:08 -0500

    added header and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route } 
+from 'react-router-dom' 
+// import Header from './Components/Header'
+import Navbar from './Components/Navbar'
+import Login from './Components/Login';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Router>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path='/home' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </div>
+      </Router>
+   </div>
   );
 }
 
